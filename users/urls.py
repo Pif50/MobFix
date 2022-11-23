0524fe9e253a7ce3e52f9ddbbf9a15cf42b6ca07
Pierfrancesco <94386461+Pif50@users.noreply.github.com>
2022-11-23 11:32:45 +0100

    Finish users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-
 from .views import registrazione_view
 
-urlpatterns = [
-    path('registrazione/', registrazione_view, name='registration_view')
-]
+urlpatterns = [path("registrazione/", registrazione_view, name="registration_view")]
