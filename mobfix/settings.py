aa75f589d3c4c0eb52cc6618ba952f139c06335d
Pierfrancesco <94386461+Pif50@users.noreply.github.com>
2022-12-09 11:28:58 +0100

    .

--- mobfix/settings.py
+++ mobfix/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 STATIC_URL = "/static/"
 
 
+MEDIA_ROOT = os.path.join((BASE_DIR), "media")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 MESSAGE_TAGS = {
